cd07df86d7e15caf97c6bce4723ccc07bc6fd6c8
aoiro27 <aoiro.savant@gmail.com>
2026-09-20 08:53:01 +0900

    風船

--- Assets/Scripts/World/BalloonProp.cs
+++ Assets/Scripts/World/BalloonProp.cs
@@ -9,6 +9,8 @@ namespace ShikiShiro
         private ProceduralSfx _sfx;
         private SpriteRenderer _sprite;
         private Collider _hitbox;
+        private Rigidbody _body;
+        private Camera _cam;
         private Vector3 _anchor;
         private float _phase;
         private float _bobSpeed;
@@ -27,6 +29,7 @@ namespace ShikiShiro
             _bobSpeed = Random.Range(0.55f, 1.15f);
             _bobAmp = Random.Range(0.18f, 0.42f);
             _spin = Random.Range(-12f, 12f);
+            _cam = Camera.main;
             transform.position = position;
             ApplySprite(sprite);
             IsAlive = true;
@@ -51,6 +54,10 @@ namespace ShikiShiro
                 hit.radius = 0.58f;
                 hit.center = Vector3.up * 0.15f;
                 _hitbox = hit;
+                _body = gameObject.AddComponent<Rigidbody>();
+                _body.isKinematic = true;
+                _body.useGravity = false;
+                _body.interpolation = RigidbodyInterpolation.Interpolate;
                 int zombie = LayerMask.NameToLayer("Zombie");
                 GameAssets.SetLayerRecursively(gameObject, zombie >= 0 ? zombie : 0);
             }
@@ -62,7 +69,7 @@ namespace ShikiShiro
             transform.localScale = Vector3.one * scale;
         }
 
-        private void LateUpdate()
+        private void FixedUpdate()
         {
             if (!IsAlive)
             {
@@ -73,18 +80,38 @@ namespace ShikiShiro
             pos.x += Mathf.Sin(Time.time * 0.21f + _phase) * 0.35f;
             pos.z += Mathf.Cos(Time.time * 0.17f + _phase) * 0.35f;
             pos.y += Mathf.Sin(Time.time * _bobSpeed + _phase) * _bobAmp;
-            transform.position = pos;
-
-            Camera cam = Camera.main;
-            if (cam != null)
+            Quaternion rot = BillboardRotation(pos);
+            if (_body != null)
             {
-                Vector3 toCam = cam.transform.position - transform.position;
-                if (toCam.sqrMagnitude > 0.001f)
-                {
-                    transform.rotation = Quaternion.LookRotation(toCam.normalized, Vector3.up)
-                        * Quaternion.Euler(0f, 0f, Mathf.Sin(Time.time * 0.8f + _phase) * _spin);
-                }
+                _body.MovePosition(pos);
+                _body.MoveRotation(rot);
             }
+            else
+            {
+                transform.SetPositionAndRotation(pos, rot);
+            }
+        }
+
+        private Quaternion BillboardRotation(Vector3 pos)
+        {
+            if (_cam == null)
+            {
+                _cam = Camera.main;
+            }
+
+            if (_cam == null)
+            {
+                return transform.rotation;
+            }
+
+            Vector3 toCam = _cam.transform.position - pos;
+            if (toCam.sqrMagnitude < 0.001f)
+            {
+                return transform.rotation;
+            }
+
+            return Quaternion.LookRotation(toCam.normalized, Vector3.up)
+                * Quaternion.Euler(0f, 0f, Mathf.Sin(Time.time * 0.8f + _phase) * _spin);
         }
 
         public void ApplyDamage(in DamageInfo info)
@@ -102,9 +129,9 @@ namespace ShikiShiro
 
             Vector3 boom = transform.position;
             Vector3 dir = info.Direction.sqrMagnitude > 0.01f ? info.Direction : Vector3.up;
-            _fx.Explosion(boom, dir, info.ChainDepth > 0 ? 0.75f : 0.95f);
-            _sfx.PlayBalloonBurst();
-            _horde.ChainBurst(boom, info);
+            _fx?.Explosion(boom, dir, info.ChainDepth > 0 ? 0.75f : 0.95f);
+            _sfx?.PlayBalloonBurst();
+            _horde?.ChainBurst(boom, info);
             gameObject.SetActive(false);
         }
     }
